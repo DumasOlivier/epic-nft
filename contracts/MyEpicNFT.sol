5d9a89a1f57a97fd0be5b67e6013c2a77f7a5f9e
DumasOlivier <dumas.olivier@outlook.fr>
2022-04-02 17:47:29 +0200

    Add NewEpicNFTMinted event

--- contracts/MyEpicNFT.sol
+++ contracts/MyEpicNFT.sol
@@ -14,6 +14,7 @@ contract MyEpicNFT is ERC721URIStorage {
 
   string baseSvg = "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base { fill: white; font-family: serif; font-size: 24px; }</style><rect width='100%' height='100%' fill='black' /><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>";
   string[] loremIpsumWords = ["Lorem", "ipsum", "dolor", "sit", "amet", "consectetur", "adipiscing", "elit.", "Vestibulum", "varius", "fermentum", "leo"];
+  event NewEpicNFTMinted(address sender, uint256 tokenId);
 
   constructor() ERC721 ("LoremIpsumNFT", "LOREM") {
     console.log("Lorem ipsum NFT contract.");
@@ -63,5 +64,7 @@ contract MyEpicNFT is ERC721URIStorage {
 
     _tokenIds.increment();
     console.log("An NFT w/ ID %s has been minted to %s", newItemId, msg.sender);
+
+    emit NewEpicNFTMinted(msg.sender, newItemId);
   }
 }
